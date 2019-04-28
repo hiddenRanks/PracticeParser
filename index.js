53265316ea4373388c91a8b663a6038ac3d13c26
ririblathmerhen <mongjja106@gmail.com>
2019-04-29 03:18:14 +0900

    19.24.28/과제 완료

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 let app = express();
 
 app.set('port', 12000);
-app.set('views', path.join(__dirname), "views");
+app.set('views', path.join(__dirname + '/views'), "views");
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,7 +17,43 @@ app.use(bodyParser.json()); //request body에 오는 데이터를 json으로 변
 app.use(bodyParser.urlencoded({extended:true})); //객체 안에 무언가를 인코딩 해야 된다면 true
 
 app.all('/', function(req, res) {
-    res.render('main', {msg: 'welcome'});
+    res.render('main', {});
+});
+
+app.get('/soulworker', function(req, res) {
+    request("https://soulworker.game.onstove.com/", function(err, response, body){ //res가 아닌 response로 한 이유는 위에 res랑 겹쳐서임
+        let NoticeList = [];
+        $ = cheerio.load(body);
+
+        let notice = $(".sectionNews .ellipsis");
+        for(let i = 0; i < notice.length; i++) {
+            let data = $(notice[i]).text();
+            NoticeList.push(data);
+        }
+
+        res.render('soulworker', {msg:'소울워커 공지사항', list:NoticeList});
+    });
+});
+
+app.get('/manamoa', function(req, res) {
+    res.render('manamoa', {list:searchList = undefined})
+});
+
+app.post('/manamoa', function(req, res) {
+    var word = req.body.word;
+    let url = "https://manamoa3.net/bbs/search.php?url=https%3A%2F%2Fmanamoa3.net%2Fbbs%2Fsearch.php&stx=" + word;
+    request(url, function(err, response, body) {
+        let searchList = [];
+        $ = cheerio.load(body);
+
+        let result = $(".post-content .manga-subject");
+        for(let i = 0; i < result.length; i++) {
+            let data = $(result[i]).text();
+            searchList.push(data);
+        }
+
+        res.render('manamoa', {msg:'제목 검색 결과', list:searchList});
+    });
 });
 
 let server = http.createServer(app);
